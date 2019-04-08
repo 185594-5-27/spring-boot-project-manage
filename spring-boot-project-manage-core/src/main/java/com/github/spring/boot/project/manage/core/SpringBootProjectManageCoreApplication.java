8717c12ac283b992c457c29a5071f39eeeef566d
林泽锋 <282245889@qq.com>
2019-04-08 10:41:02 +0800

    Initial commit

---
+++ spring-boot-project-manage-core/src/main/java/com/github/spring/boot/project/manage/core/SpringBootProjectManageCoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.spring.boot.project.manage.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectManageCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectManageCoreApplication.class, args);
+    }
+
+}
